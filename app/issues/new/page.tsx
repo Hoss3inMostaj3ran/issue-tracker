552aee75fda61571c635787fc50600a853df7d7d
Hoss3inMostaj3ran <98212611+Hoss3inMostaj3ran@users.noreply.github.com>
2024-03-12 11:14:31 +0330

    add simple markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,4 +1,15 @@
+"use client";
+import React, { useCallback, useState } from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
 const NewIssue = () => {
+  const [value, setValue] = useState(""); // State to store Markdown content
+
+  const onChange = useCallback((value: string) => {
+    setValue(value);
+  }, []);
+
   return (
     <form className="p-5">
       <h1 className="text-2xl my-5">What is your Issue? </h1>
@@ -8,9 +19,11 @@ const NewIssue = () => {
           placeholder="Enter Title"
           className="input input-bordered  w-full max-w-3xl"
         />
-        <textarea
+        <SimpleMDE
           placeholder="Enter Description"
-          className="textarea textarea-bordered textarea-md  w-full h-15 max-w-3xl"
+          className="w-full h-15 max-w-3xl"
+          value={value}
+          onChange={onChange}
         />
       </div>
       <button className="btn bg-gray-300 hover:bg-gray-400 mt-5" type="submit">
