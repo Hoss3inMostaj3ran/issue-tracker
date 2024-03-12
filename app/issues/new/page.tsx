99d8ea66953fa93665430773f3e82a3ffcc642cc
Hoss3inMostaj3ran <98212611+Hoss3inMostaj3ran@users.noreply.github.com>
2024-03-12 12:08:24 +0330

    Handle Form Submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,35 +1,56 @@
 "use client";
-import React, { useCallback, useState } from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssue = () => {
-  const [value, setValue] = useState(""); // State to store Markdown content
-
-  const onChange = useCallback((value: string) => {
-    setValue(value);
-  }, []);
-
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
   return (
-    <form className="p-5">
-      <h1 className="text-2xl my-5">What is your Issue? </h1>
-      <div className="flex flex-col gap-4">
-        <input
-          type="text"
-          placeholder="Enter Title"
-          className="input input-bordered  w-full max-w-3xl"
-        />
-        <SimpleMDE
-          placeholder="Enter Description"
-          className="w-full h-15 max-w-3xl"
-          value={value}
-          onChange={onChange}
-        />
-      </div>
-      <button className="btn bg-gray-300 hover:bg-gray-400 mt-5" type="submit">
-        Submit Issue
-      </button>
-    </form>
+    <>
+      <h1 className="text-2xl mt-5 pt-2 mx-5">What is your Issue? </h1>
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          await axios.post("/api/issues", data);
+          router.push("/issues");
+          console.log("success");
+        })}
+        className="p-5"
+      >
+        <div className="flex flex-col gap-4">
+          <input
+            {...register("title")}
+            type="text"
+            placeholder="Enter Title"
+            className="input input-bordered  w-full max-w-3xl"
+          />
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <SimpleMDE
+                {...field}
+                placeholder="Enter Description"
+                className="w-full h-15 max-w-3xl"
+              />
+            )}
+          />
+        </div>
+        <button
+          className="btn bg-gray-300 hover:bg-gray-400 mt-5"
+          type="submit"
+        >
+          Submit Issue
+        </button>
+      </form>
+    </>
   );
 };
 
