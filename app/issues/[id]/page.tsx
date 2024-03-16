12267045ecd8240a0ea433f07618ac776fcd4bfd
Hoss3inMostaj3ran <98212611+Hoss3inMostaj3ran@users.noreply.github.com>
2024-03-16 14:29:09 +0330

    Refactor : Fix IssueDetailsPage UI

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -17,59 +17,36 @@ const IssuesDetailPage = async ({ params: { id } }: Props) => {
   if (!issue) notFound();
 
   return (
-    <div>
-      <div className="stats shadow flex lg:flex-row max-lg:flex-col">
-        <div className="stat flex-1">
-          <div className="stat-figure text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-8 h-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
+    <div className="flex flex-col  gap-7">
+      <div id="capilot">
+        <div className="container mx-auto p-4">
+          <div className="bg-white rounded-lg shadow-md p-6">
+            <h1 className="text-2xl font-medium mb-4">Issue Details</h1>
+            <p className="text-gray-600 mb-2 font-medium">
+              Issue ID: <span className="text-blue-500">{issue?.id}</span>
+            </p>
+            <p className="text-gray-600 mb-2 font-medium">
+              Issue Title: <span className="text-blue-500">{issue?.title}</span>
+            </p>
+            <p className="text-gray-600 mb-2 font-medium">
+              Status:{" "}
+              <span className="text-green-500 mx-3">
+                {<StatusBadge status={issue.status} />}
+              </span>
+            </p>
+            <p className="text-gray-600 mb-2 font-medium">
+              Created Date:{" "}
+              <span className="text-blue-500">
+                {issue?.createdAt.toDateString()}
+              </span>
+            </p>
+            <p className="text-gray-600 mb-2 font-medium">
+              Description:{" "}
+              <ReactMarkdown className="text-gray-800 prose ">
+                {issue?.description}
+              </ReactMarkdown>
+            </p>
           </div>
-          <div className="stat-title">Issue ID</div>
-          <div className="stat-value">{issue?.id}</div>
-        </div>
-        <div className="stat flex-1">
-          <div className="stat-figure text-secondary">
-            {<StatusBadge status={issue.status} />}
-          </div>
-          <div className="stat-figure text-secondary"></div>
-          <div className="stat-title">Title :</div>
-          <div className="stat-value">{issue?.title}</div>
-        </div>
-        <div className="stat flex-1">
-          <div className="stat-figure text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-8 h-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-              ></path>
-            </svg>
-          </div>
-          <div className="stat-title">Created Date :</div>
-          <div className="stat-value">{issue?.createdAt.toDateString()}</div>
-        </div>
-        <div className="stat flex-auto">
-          <div className="stat-title">Description :</div>
-          <ReactMarkdown className="stat prose">
-            {issue?.description}
-          </ReactMarkdown>
         </div>
       </div>
     </div>
