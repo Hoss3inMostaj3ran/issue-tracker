1a1c2bb78245fa07a7cde32a19ea3b0fe3e94e3a
Hoss3inMostaj3ran <98212611+Hoss3inMostaj3ran@users.noreply.github.com>
2024-03-21 16:51:34 +0330

    Show Notification

--- app/issues/[id]/SelectorClient.tsx
+++ app/issues/[id]/SelectorClient.tsx
@@ -5,6 +5,7 @@ import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Skeleton from "react-loading-skeleton";
+import toast, { Toaster } from "react-hot-toast";
 
 const SelectorClient = ({ issue }: { issue: Issue }) => {
   const {
@@ -19,6 +20,7 @@ const SelectorClient = ({ issue }: { issue: Issue }) => {
   });
 
   if (isLoading) return <Skeleton height="40px" />;
+  if (error) console.log(error);
 
   const checkId = (id: null | "unassigned" | string) => {
     if (id === "unassigned") return null;
@@ -27,30 +29,39 @@ const SelectorClient = ({ issue }: { issue: Issue }) => {
   };
 
   return (
-    <Select.Root
-      defaultValue={issue.userId || "unassigned"}
-      size={"3"}
-      onValueChange={(id) => {
-        axios.patch(`/api/issues/${issue.id}`, { userId: checkId(id) });
-      }}
-    >
-      <Select.Trigger
-        className="select select-primary select-bordered border-2 w-full max-w-xs"
-        radius="none"
-        placeholder="Assign user..."
-      />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="unassigned">Unassigned</Select.Item>
-          {users?.map((u) => (
-            <Select.Item key={u.id} value={u.id}>
-              {u.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.userId || "unassigned"}
+        size={"3"}
+        onValueChange={async (id) => {
+          try {
+            await axios.patch(`/api/issues/${issue.id}`, {
+              userId: checkId(id),
+            });
+          } catch (error) {
+            toast.error("Change could not be saved");
+          }
+        }}
+      >
+        <Select.Trigger
+          className="select select-primary select-bordered border-2 w-full max-w-xs"
+          radius="none"
+          placeholder="Assign user..."
+        />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            {users?.map((u) => (
+              <Select.Item key={u.id} value={u.id}>
+                {u.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
