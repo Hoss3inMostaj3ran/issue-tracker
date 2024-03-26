2dabd8a10747a0826771c934ff316066d5dda7c4
Hoss3inMostaj3ran <98212611+Hoss3inMostaj3ran@users.noreply.github.com>
2024-03-26 13:55:15 +0330

    Build issue summery page

---
+++ app/dashboard/IssuesCount.tsx
@@ -0,0 +1,50 @@
+import { Status } from "@prisma/client";
+import { Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import StatusBadge from "../components/StatusBadge";
+
+type Props = {
+  opens: number;
+  closes: number;
+  inProgresses: number;
+};
+
+const IssuesCount = ({ opens, closes, inProgresses }: Props) => {
+  const issuesSummery: {
+    lable: string;
+    value: number;
+    status: Status;
+  }[] = [
+    { lable: "Open Issues", value: opens, status: "OPEN" },
+    { lable: "In-Progress Issues", value: inProgresses, status: "IN_PROGRESS" },
+    { lable: "Closed Issues", value: closes, status: "CLOSE" },
+  ];
+
+  return (
+    <div className="">
+      <Flex direction={"row"} justify={"between"} gap={"3"}>
+        {issuesSummery.map((i) => (
+          <Flex
+            key={i.value}
+            direction={"column"}
+            gap={"2"}
+            className="border-2  rounded-xl border-slate-400 w-full hover:border-gray-600"
+          >
+            <div className="mx-2 my-2 p-2">
+              <Flex direction={"row"} align={"center"} justify={"between"}>
+                <h3 className="font-semibold text-lg tracking-wider">
+                  <Link href={`/issues?status=${i.status}`}>{i.lable}</Link>
+                </h3>
+                <StatusBadge status={i.status} />
+              </Flex>
+
+              <Text className="font-bold font-serif text-3xl">{i.value}</Text>
+            </div>
+          </Flex>
+        ))}
+      </Flex>
+    </div>
+  );
+};
+
+export default IssuesCount;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,16 @@
+import prisma from "@/prisma/client";
+import IssuesCount from "./IssuesCount";
 import LatestIssues from "./LatestIssues";
 
-export default function page() {
-  return <LatestIssues />;
+export default async function page() {
+  const closes = await prisma.issue.count({ where: { status: "CLOSE" } });
+  const opens = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgresses = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  return (
+    <div className="">
+      <IssuesCount closes={closes} opens={opens} inProgresses={inProgresses} />
+    </div>
+  );
 }
